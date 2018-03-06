b68367a80b3859c120c02c2a26100153a1b7bcb9
Pavel Petržela <pavel@petrzela.eu>
2018-03-06 22:40:48 +0100

    Update ReinitializeGit.sh

--- ReinitializeGit.sh
+++ ReinitializeGit.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]
-  then
-    echo -e "Invalid number of arguments!\nInput should be like\nReinitializeGit.sh <Project-Directory> <SSH-Cloning-Link>"
-    exit 1
-  elif [ ! -d "$1" ]
-    then
-      echo -e "First argument must be the directory you want to reinitialize"
-      exit 1
-  else
-    cd "$1"
-    rm -rf .git
-    git init
-    git remote add origin "$2"
-    if [ "$?" -ne 0 ]
-      then
-        echo -e "Error occurred while connecting git... Exiting :("
-        exit 1
-      else
-        git remote -v
-        git add --all
-        git commit -am "Reinitialization commit"
-        git push --force --set-upstream origin master
-      fi
-  fi
+
+[ "$#" -eq 2 ] \
+  || { echo -e "Invalid number of arguments!\nInput should be like\nReinitializeGit.sh <Project-Directory> <SSH-Cloning-Link>"; exit 1; }
+
+[ -d "$1" ] \
+  || { echo -e "First argument must be the directory you want to reinitialize"; exit 1; } 
+
+rm -rf .git \
+  && git -C "$1" init \
+  && git -C "$1" remote add origin "$2"
+  || { echo -e "Error occurred while connecting git... Exiting :("; exit 1; }
+
+git -C "$1" remote -v \
+ && git -C "$1" add --all \
+ && git -C "$1" commit -am "Reinitialization commit" \
+ && git -C "$1" push --force --set-upstream origin master \
+ || { echo -e "Err.. Exiting :("; exit 1; }
